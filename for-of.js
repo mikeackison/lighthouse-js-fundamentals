6c1e023337be5d023e250f104d49b2af686c6d7b
Mike Ackison <mike.ackison@shopify.com>
2020-12-08 22:45:10 +0000

    add for-f practice and llopyLighthouse scratch

---
+++ for-of.js
@@ -0,0 +1,23 @@
+const amounts = [61.00, 52.25, 112.99, 5.00]; 
+
+// let total = 0
+
+// for (let i = 0; i < amounts.length; i++) {
+//   total += amounts[i];
+// }
+
+// console.log('Order total is ' + total)
+
+let total = 0
+
+for (let amount of amounts) {
+  total += amount
+}
+
+console.log('Order total is $' + total);
+
+let data = ['mike']
+
+for (let i of data) {
+  console.log(i);
+}
