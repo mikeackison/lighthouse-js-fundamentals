22ca1768ce5ef40b904feba7cb18f7a551bb2cfd
Mike Ackison <mike.ackison@shopify.com>
2020-12-08 22:53:06 +0000

    add even function

---
+++ even.js
@@ -0,0 +1,10 @@
+const isEven = function (number) {
+  return number % 2 === 0;
+}
+
+// const tenIsEven = isEven(10);
+
+// const elevenIsEven = isEven(11);
+
+console.log(isEven(10));
+console.log(isEven(11));
