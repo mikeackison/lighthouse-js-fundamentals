5a8e072690f8d32eea72dd054d94a172bf5155f5
Mike Ackison <mike.ackison@shopify.com>
2020-12-08 15:14:43 +0000

    Add more conditional examples

--- conditionals.js
+++ conditionals.js
@@ -1,24 +1,32 @@
-// const raining = true;
-// const cold = true;
 
-// if (raining) {
-//   console.log("Don't forget your umbrella!");
-// }
+//if statements
+// EXAMPLE 1
+const raining = false;
+const cold = true;
 
-// if (cold) {
-//   console.log("Make sure you pick out a scarf!");
-// }
+if (raining) {
+  console.log("Don't forget your umbrella!");
+}
 
-// console.log("Now you're ready to go outside.")
+if (cold) {
+  console.log("Make sure you pick out a scarf!");
+}
 
-// const cold = false;
+console.log("Now you're ready to go outside.")
 
-// if (cold) {
-//   console.log("Make sure you pick out a scarf!");
-// } else {
-//   console.log("Short sleeves are fine.");
-// }
 
+
+// temperature example
+// EXAMPLE 2
+
+if (cold) {
+  console.log("Make sure you pick out a scarf!");
+} else {
+  console.log("Short sleeves are fine.");
+}
+
+// temperature example using numbers
+// EXAMPLE 3
 const temp = -1;
 
 if (temp < 0) {
@@ -27,4 +35,29 @@ if (temp < 0) {
   console.log("Short sleeves won't cut it.");
 } else {
   console.log("Short sleeves are fine!");
+}
+
+
+// Logical Operators
+// EXAMPLE 1
+
+const isCitizen = true;
+const age = 26;
+
+if (isCitizen && age > 18) {
+  console.log("You are elligable to vote.")
+}
+
+// EXAMPLE 2 
+
+const temperature = 41
+
+if (temperature < -40 || temperature > 40) {
+  console.log("Maybe going outside isn't such a great idea…");
+}
+
+// EXAMPLE 3 
+
+if (!raining) {
+  console.log("Leave your umbrella at home!");
 }
