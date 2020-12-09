3f53894d8f7fa49726852dc628baa0bde9302248
Mike Ackison <mike.ackison@shopify.com>
2020-12-09 22:29:44 +0000

    add while loop, for loop, and backwards for loop.

---
+++ ingredients.js
@@ -0,0 +1,37 @@
+const ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+
+// Write a while loop that prints out the contents of ingredients:
+
+
+console.log("Banana Bread Ingredients - WHILE")
+
+let i = 0
+
+while (i < ingredients.length) {
+  console.log(ingredients[i])
+  i++
+}
+
+
+// Write a for loop that prints out the contents of ingredients:
+
+console.log("Banana Bread Ingredietns - FOR LOOP")
+
+for (let i of ingredients) {
+  console.log(i)
+}
+
+// OR
+
+// for (let i = 0; i < ingredients.length; i++) {
+//   console.log(ingredients[i])
+// }
+
+
+// Write any loop (while or for) that prints out the contents of ingredients backwards:
+
+console.log("Banana Bread Ingredietns - BACKWARDS")
+
+for (let i = ingredients.length - 1; i > 0; i--) {
+  console.log(ingredients[i]);
+}
