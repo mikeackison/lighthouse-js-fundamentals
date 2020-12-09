776c46c38e6960b05153919354752a7e148441b0
Mike Ackison <mike.ackison@shopify.com>
2020-12-09 22:04:38 +0000

    add iterating over and array with while

--- packing-list.js
+++ packing-list.js
@@ -1,9 +1,16 @@
 const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
 
+// console.log("Kitchen stuff to pack:");
+// for (let i = 0; i < packingList.length; i++) {
+//   console.log(packingList[i]);
+// } 
+
+// console.log(packingList[3]);
+
+
 console.log("Kitchen stuff to pack:");
-for (let i = 0; i < packingList.length; i++) {
+let i = 0;
+while (i < packingList.length) {
   console.log(packingList[i]);
+  i++;
 }
-
-console.log(packingList[3]);
-
